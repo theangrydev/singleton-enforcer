6859edf8aa8d3e736fca710d2226f5ec7e3cfff3
Liam Williams <liam.williams@zoho.com>
2016-07-10 15:18:54 +0100

    Seperate surefire execution for the singleton test

---
+++ src/test/java/io/github/theangrydev/singletonenforcer/AnotherTest.java
@@ -0,0 +1,12 @@
+package io.github.theangrydev.singletonenforcer;
+
+import org.junit.Test;
+
+public class AnotherTest {
+
+    @Test
+    public void another() {
+        new SomeClass();
+        System.out.println("true = " + true);
+    }
+}
---
+++ src/test/java/io/github/theangrydev/singletonenforcer/SomeClass.java
@@ -0,0 +1,8 @@
+package io.github.theangrydev.singletonenforcer;
+
+public class SomeClass {
+
+    public SomeClass() {
+        System.out.println("SomeClass = " + true);
+    }
+}
